9226e2613c21ccc74d5a75854406722474db73db
Iryna Kravchenko <kravchenkoiryna1207@gmail.com>
2026-02-24 10:58:18 +0100

    initial commit 1

--- src/main/java/mate.academy/lib/Injector.java
+++ src/main/java/mate.academy/lib/Injector.java
@@ -1,13 +1,50 @@
 package mate.academy.lib;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.util.HashSet;
+import java.util.Set;
+
 public class Injector {
     private static final Injector injector = new Injector();
-
+    private final Set<Class<?>> creating = new HashSet<>();
     public static Injector getInjector() {
         return injector;
     }
 
     public Object getInstance(Class<?> interfaceClazz) {
-        return null;
+        if (!interfaceClazz.isAnnotationPresent(Component.class)) {
+            throw new RuntimeException("Can't find the annotation!");
+        }
+        if (creating.contains(interfaceClazz)) {
+            throw new RuntimeException("Cyclic dependency detected: " + interfaceClazz);
+        }
+        creating.add(interfaceClazz);
+        Object instance;
+        try {
+            Constructor<?> constructor = interfaceClazz.getDeclaredConstructor();
+            constructor.setAccessible(true);
+            instance = constructor.newInstance();
+            for (Field field : interfaceClazz.getDeclaredFields()) {
+                if (!field.isAnnotationPresent(Inject.class)) continue;
+                Class<?> fieldType = field.getType();
+                Class<?> impl = fieldType;
+                Object dependency = getInstance(impl);
+                field.setAccessible(true);
+                field.set(instance, dependency);
+            }
+        } catch (NoSuchMethodException e) {
+            throw new RuntimeException(e);
+        } catch (InstantiationException e) {
+            throw new RuntimeException(e);
+        } catch (IllegalAccessException e) {
+            throw new RuntimeException(e);
+        } catch (InvocationTargetException e) {
+            throw new RuntimeException(e);
+        } finally {
+            creating.remove(interfaceClazz);
+        }
+        return instance;
     }
 }
--- src/main/java/mate.academy/model/Product.java
+++ src/main/java/mate.academy/model/Product.java
@@ -1,12 +1,20 @@
 package mate.academy.model;
 
+import mate.academy.lib.Component;
+import mate.academy.lib.Inject;
+
 import java.math.BigDecimal;
 
 public class Product {
+    @Inject
     private Long id;
+    @Inject
     private String name;
+    @Inject
     private String category;
+    @Inject
     private String description;
+    @Inject
     private BigDecimal price;
 
     public Long getId() {
